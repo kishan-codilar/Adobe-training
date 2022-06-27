6e362a0adc784df8b925aa21b2fd347fe27929bb
kishan-codilar <94516737+kishan-codilar@users.noreply.github.com>
2022-06-27 13:25:47 +0530

    updated

--- Unit1/SimpleUiComponent/Controller/Adminhtml/UiComponent/Index.php
+++ Unit1/SimpleUiComponent/Controller/Adminhtml/UiComponent/Index.php
@@ -3,18 +3,19 @@ namespace Unit1\SimpleUiComponent\Controller\Adminhtml\UiComponent;
 
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Backend\App\Action;
+use Magento\Framework\Controller\ResultInterface;
 
 class Index extends Action
 {
     /**
      * ACL access restriction
      */
-    const ADMIN_RESOURCE = 'Unit1_SimpleUiComponent::simple_list';
+     const ADMIN_RESOURCE = 'Unit1_SimpleUiComponent::simple_list';
 
     /**
      * View page action
      *
-     * @return \Magento\Framework\Controller\ResultInterface
+     * @return ResultInterface
      */
     public function execute()
     {
