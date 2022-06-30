52109cb5b40a4f9a5519814f4882e6e058cda9ae
kishan-codilar <94516737+kishan-codilar@users.noreply.github.com>
2022-06-30 20:44:09 +0530

    coupon invalidation

---
+++ Kishan/Assignment5/Model/CouponManagement.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ *
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Kishan\Assignment5\Model;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+/**
+ * Coupon management object.
+ */
+class CouponManagement extends \Magento\Quote\Model\CouponManagement
+{
+    /**
+     * @inheritDoc
+     */
+    public function set($cartId, $couponCode)
+    {
+        /** @var  \Magento\Quote\Model\Quote $quote */
+        $quote = $this->quoteRepository->getActive($cartId);
+        if (!$quote->getItemsCount()) {
+            throw new NoSuchEntityException(__('The "%1" Cart doesn\'t contain products.', $cartId));
+        }
+        if (!$quote->getStoreId()) {
+            throw new NoSuchEntityException(__('Cart isn\'t assigned to correct store'));
+        }
+        $quote->getShippingAddress()->setCollectShippingRates(true);
+
+        try {
+//            $quote->setCouponCode($couponCode);
+            $this->quoteRepository->save($quote->collectTotals());
+        } catch (LocalizedException $e) {
+            throw new CouldNotSaveException(__('The coupon code couldn\'t be applied: ' .$e->getMessage()), $e);
+        } catch (\Exception $e) {
+            throw new CouldNotSaveException(
+                __("The coupon code couldn't be applied. Verify the coupon code and try again."),
+                $e
+            );
+        }
+        if ($quote->getCouponCode() != $couponCode) {
+            throw new NoSuchEntityException(__("The coupon code is not Valid."));
+        }
+        return true;
+    }
+}
