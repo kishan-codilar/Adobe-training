f3b7f2027c3d011d5d8ebfe5eeaebe788b1436d7
kishan-codilar <94516737+kishan-codilar@users.noreply.github.com>
2022-07-01 13:08:37 +0530

    made some changes

--- Kishan/Assignment5/Model/CouponManagement.php
+++ Kishan/Assignment5/Model/CouponManagement.php
@@ -23,26 +23,8 @@ class CouponManagement extends \Magento\Quote\Model\CouponManagement
     {
         /** @var  \Magento\Quote\Model\Quote $quote */
         $quote = $this->quoteRepository->getActive($cartId);
-        if (!$quote->getItemsCount()) {
-            throw new NoSuchEntityException(__('The "%1" Cart doesn\'t contain products.', $cartId));
-        }
-        if (!$quote->getStoreId()) {
-            throw new NoSuchEntityException(__('Cart isn\'t assigned to correct store'));
-        }
-        $quote->getShippingAddress()->setCollectShippingRates(true);
-
-        try {
-//            $quote->setCouponCode($couponCode);
-            $this->quoteRepository->save($quote->collectTotals());
-        } catch (LocalizedException $e) {
-            throw new CouldNotSaveException(__('The coupon code couldn\'t be applied: ' .$e->getMessage()), $e);
-        } catch (\Exception $e) {
-            throw new CouldNotSaveException(
-                __("The coupon code couldn't be applied. Verify the coupon code and try again."),
-                $e
-            );
-        }
-        if ($quote->getCouponCode() != $couponCode) {
+        $code = $quote->setCouponCode($couponCode);
+        if ($code) {
             throw new NoSuchEntityException(__("The coupon code is not Valid."));
         }
         return true;
