457088f8c11ed12b38baa5ae1958364db4e94213
kishan-codilar <94516737+kishan-codilar@users.noreply.github.com>
2022-07-22 15:54:34 +0530

    displaying customer name after add to cart

---
+++ Kishan/Assignment7/CustomerData/CustomerData.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Kishan\Assignment7\CustomerData;
+
+use Magento\Customer\Model\Session;
+use Magento\Customer\CustomerData\SectionSourceInterface;
+
+class CustomerData implements SectionSourceInterface
+{
+    /**
+     * @var Session
+     */
+    private Session $session;
+
+    /**
+     * CustomerData constructor.
+     * @param Session $session
+     */
+    public function __construct(
+        Session $session
+    ) {
+        $this->session=$session;
+    }
+
+    /**
+     * Return Array with data for a private block
+     *
+     * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function getSectionData()
+    {
+        return [
+            "customername" => $this->session->getCustomer()->getName(),
+        ];
+    }
+}
