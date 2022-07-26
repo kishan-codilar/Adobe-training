ca90ab973c6bf0c1da5ebb771e82323fe3b809eb
kishan-codilar <94516737+kishan-codilar@users.noreply.github.com>
2022-07-26 10:38:15 +0530

    display customer name below add to cart button

--- Kishan/Assignment7/registration.php
+++ Kishan/Assignment7/registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
- ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Kishan_Assignment7', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Kishan_Assignment7', __DIR__);
